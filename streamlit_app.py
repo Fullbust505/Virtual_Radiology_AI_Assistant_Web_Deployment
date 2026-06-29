557a2cc7d640a7ea329d58f1b18c8ce3332ace1c
Fullbust505 <117484278+Fullbust505@users.noreply.github.com>
2026-06-29 11:27:25 +0200

    coordination push

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
-import streamlit as st 
+import streamlit as st
 from PIL import Image
 
 # title
-st.title("IA Radio Assistant")
+st.title("IA Radio Assistant", text_alignment="center")
 
 st.markdown('''
 Hi ! Welcome to your new AI Radiology Assistant. The purpose of this AI is to analyze images and make a pseudo-diagnostic over it.<br>
@@ -12,9 +12,11 @@ You can do 2 things here : <br>
 Enjoy the tool ! 
 ''', unsafe_allow_html=True)
 
+# image handling
 file_png = st.file_uploader("Upload a PNG image", type=["png", "jpg", "jpeg"])
 
 if file_png is not None:
     img = Image.open(file_png)
     st.image(img)
+
 
